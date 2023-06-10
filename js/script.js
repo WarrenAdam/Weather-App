1e35589e3e0b993338772f825bd6a0dd1b17329f
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-06-10 08:33:42 -0400

    added error div when user inputs wrong city name. They will also recieve an error message.

--- js/script.js
+++ js/script.js
@@ -7,11 +7,12 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather() {
     const response = await fetch(apiUrl + `&appid={API key}`);
-    var data = await response.json();
 
-    console.log(data);
-
-    document.querySelector(".city").innerHTML = data.name;
+    if (response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+        
+    } else {  document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
@@ -32,6 +33,15 @@ async function checkWeather() {
             weatherIcon.src = "./Images/mist.png";
     }
 
+    document.querySelector(".weather").style.display = "block";
+
+    }
+
+    var data = await response.json();
+
+    console.log(data);
+
+   
 }
 
 
